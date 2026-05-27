07b4355c4561d70430d0da7c128aa750aa4d649b
Regis Tsafack <regis.tsafack@yahoo.com>
2026-05-27 02:25:26 +0200

    feat: complete FunctionPlotter app with signal generator and GUI

--- +src/SignalGenerator.m
+++ +src/SignalGenerator.m
@@ -1,27 +1,25 @@
 classdef SignalGenerator < handle
-
     properties
-        frequency = 1
-        funcType = 'sin'
+        frequency = 1      % Hz
+        funcType  = 'sin' 
     end
 
     events
-        NewDataGenerated
+        NewDataReady    
     end
 
     methods
         function generate(obj)
-            t= linspace(o, 1, 1000)
+            t = linspace(0, 1, 1000); 
 
             if strcmp(obj.funcType, 'sin')
-                y= sin(2*pi*obj.funcType * t);
+                y = sin(2 * pi * obj.frequency * t); 
             else
-                y= cos(2*pi*obj.funcType * t);
+                y = cos(2 * pi * obj.frequency * t); 
             end
 
             data = src.SignalData(t, y);
-            notify (obj, 'NewDataReady', data);
+            notify(obj, 'NewDataReady', data);
         end
     end
-
 end
